0de30039843a2584f57d82f767c9b015987add93
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-04-06 22:16:32 +0000

    updated #03 test

--- exercises/03-What-is-a-function/tests.py
+++ exercises/03-What-is-a-function/tests.py
@@ -1,14 +1,6 @@
 import io, sys, pytest, os, re, mock
 
 
-@pytest.mark.it("Create a function 'sum'")
-def test_declare_variable():
-    path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
-    with open(path, 'r') as content_file:
-        content = content_file.read()
-        regex = re.compile(r"def(\s*)sum\(")
-        assert bool(regex.search(content)) == True
-
 @pytest.mark.it('The function sum must exist')
 def test_for_callable(capsys, app):
     assert callable(app.sum)
@@ -21,10 +13,11 @@ def test_for_integer(capsys, app):
 def test_for_return(capsys, app):
     assert app.sum(3,4) == 7
 
-@pytest.mark.it('The variable super_duper needs to be declared')
+@pytest.mark.it('You need to define the variable super_duper')
 def test_for_existence(capsys, app):
     app.super_duper
 
 @pytest.mark.it('The variable super_duper needs to have as value the result of the sum of the two numbers')
 def test_for_file_output(capsys, app):
     assert app.super_duper == (3445324+53454423)
+
