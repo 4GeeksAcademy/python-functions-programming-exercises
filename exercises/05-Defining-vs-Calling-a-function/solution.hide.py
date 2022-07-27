63bac30725112b16ac01dbd517ac228ec0a31093
dsilva06 <diegosilva06092001@gmail.com>
2022-07-27 20:14:02 +0000

    test

---
+++ exercises/05-Defining-vs-Calling-a-function/solution.hide.py
@@ -0,0 +1,10 @@
+# Define the function called "multi" that expects 2 parameters:
+def multi(num1 , num2):
+    total = num1 * num2
+    print(total)
+    return total
+
+
+# don't edit anything below this line
+return_value = multi(7,53812212)
+print(return_value)
