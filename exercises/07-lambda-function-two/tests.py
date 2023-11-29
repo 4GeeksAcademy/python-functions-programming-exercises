daa03e4b741f1daca80b997194fdcc5580132b58
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 02:16:33 +0100

    Update tests.py

--- exercises/07-lambda-function-two/tests.py
+++ exercises/07-lambda-function-two/tests.py
@@ -1,6 +1,6 @@
 import io, sys, pytest, os, re, mock
 
-@pytest.mark.it("Declare a function 'rapid' as lambda")
+@pytest.mark.it("Declare a function called 'rapid' as lambda")
 def test_declare_variable():
     path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
     with open(path, 'r') as content_file:
@@ -12,7 +12,7 @@ def test_declare_variable():
 def test_for_callable(capsys):
     from app import rapid
 
-@pytest.mark.it('The function rapid must receive one string and return the same but without the last letter (make sure it\'s lowecase)')
+@pytest.mark.it('The function rapid must receive one string and return the same string without the last character')
 def test_for_integer(capsys):
     from app import rapid
     assert rapid("maria") == "mari"
