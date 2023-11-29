eaefd88b6d4fec91a545ffe8b80e167f2b6891a6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 02:34:31 +0100

    Update app.py

--- exercises/08-Function-that-returns/app.py
+++ exercises/08-Function-that-returns/app.py
@@ -1,7 +1,7 @@
 def dollar_to_euro(dollar_value):
-	return dollar_value * 0.89
+	return dollar_value * 0.91
 
 def euro_to_yen(euro_value):
-	return euro_value * 124.15
+	return euro_value * 161.70
 
 ####### ↓ YOUR CODE BELOW ↓ #######
