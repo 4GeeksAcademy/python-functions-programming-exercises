a18d892882154e6a8ec105afbc9a1ef2e3a012bf
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 02:11:54 +0100

    Update app.py

--- exercises/07-lambda-function-two/app.py
+++ exercises/07-lambda-function-two/app.py
@@ -1,5 +1,5 @@
 
 
 
-# From this line above, plese do not change code below
-print(rapid("bob")) #should print bo
+# Your code above, please do not change code below
+print(rapid("bob")) # Should print "bo"
