d9c07a0e0dc42ec0389be8d10f63cadd852f02ac
Catherine Cazorla <cathycaz00@gmail.com>
2025-09-23 18:03:13 +0000

    Pending changes exported from your codespace

--- .learn/resets/06-lambda-function-two/app.py
+++ .learn/resets/06-lambda-function-two/app.py
@@ -1,4 +1,4 @@
-rapid= lambda nombre: nombre[:-1]
+
 
 
 # Your code above, please do not change code below
--- .learn/resets/09-Array-Methods/app.py
+++ .learn/resets/09-Array-Methods/app.py
@@ -1,7 +1,6 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 
 ## CREATE YOUR FUNCTION HERE
-def sort_names(lista):
-    return sorted(lista)
+
 
 print(sort_names(names))
